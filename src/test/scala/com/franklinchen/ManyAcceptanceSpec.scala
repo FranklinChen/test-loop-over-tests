30e803aef95f63d1d736378d38a594c3722656ba
Franklin Chen <franklinchen@franklinchen.com>
2014-02-06 22:37:19 -0500

    Clean up formatting of generated specs2 acceptance spec.

--- src/test/scala/com/franklinchen/ManyAcceptanceSpec.scala
+++ src/test/scala/com/franklinchen/ManyAcceptanceSpec.scala
@@ -3,18 +3,16 @@ package com.franklinchen
 import org.specs2._
 import org.specs2.specification.Fragments
 
-class ManyAcceptanceSpec extends Specification { def is = s2"""
-  A bunch of generated specs2 tests $e1
-  """
-
-  // Not as clean as the unit-style way.
-  def e1 = {
+class ManyAcceptanceSpec extends Specification {
+  def is = {
     Seq(1, 3, 5).
-      foldLeft("A bunch of generated specs2 tests should": Fragments) {
+      foldLeft(p ^ "A bunch of generated specs2 tests should" ^ p) {
         (res, i) =>
-        res ^
-        "test each integer " + i + " for oddness" !
-        { i % 2 ==== 1 }
-      }
+        res ^ br ^ testInt(i)
+      } ^ endp
+  }
+
+  def testInt(i: Int) = {
+    s"test each integer $i for oddness" ! { i % 2 ==== 1 }
   }
 }
