aef9e1e719ae570eb3be2ee47f0b16aee0f6194a
Franklin Chen <franklinchen@franklinchen.com>
2017-07-27 15:44:39 -0400

    Update versions. Run scalafmt.

--- src/test/scala/com/franklinchen/ManyAcceptanceSpec.scala
+++ src/test/scala/com/franklinchen/ManyAcceptanceSpec.scala
@@ -6,9 +6,9 @@ import org.specs2.specification.core.Fragments
 class ManyAcceptanceSpec extends Specification {
   def is = {
     p ^ "A bunch of generated immutable specs2 tests should" ^ br ^
-    Fragments.foreach(Seq(1, 3, 5)) { i =>
-      testInt(i) ^ br
-    }
+      Fragments.foreach(Seq(1, 3, 5)) { i =>
+        testInt(i) ^ br
+      }
   }
 
   def testInt(i: Int) =
